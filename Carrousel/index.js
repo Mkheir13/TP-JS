81b9115a39b46163d40f5630832d6acebd2e3c74
Mkheir13 <kheireddine.mederreg@ynov.com>
2022-06-10 17:34:37 +0200

    exercise 1

---
+++ Carrousel/index.js
@@ -0,0 +1,33 @@
+const source = document.querySelector(" video > source");
+source.src = "vids/highlightBulls.mp4";
+
+function Bulls() {
+    document.getElementById("vid").src = "vids/highlightBulls.mp4";
+    document.getElementById("team").src = "img/bulls.png";
+
+}
+
+function Celtics() {
+    document.getElementById("vid").src = "vids/highlightCeltics.mp4";
+    document.getElementById("team").src = "img/celtics.png";
+}
+
+function Bucks() {
+    document.getElementById("vid").src = "vids/highlightBucks.mp4";
+    document.getElementById("team").src = "img/bucks.png";
+}
+
+function Raptors() {
+    document.getElementById("vid").src = "vids/highlightRaptors.mp4";
+    document.getElementById("team").src = "img/raptors.png";
+}
+
+function Lakers() {
+    document.getElementById("vid").src = "vids/highlightLakers.mp4";
+    document.getElementById("team").src = "img/lakers.png";
+}
+
+
+
+
+
