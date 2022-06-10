ec3b465563c98d5a9d43f08e67cea52fa5d5a3a1
Mkheir13 <kheireddine.mederreg@ynov.com>
2022-06-10 13:53:52 +0200

    exercise 2

---
+++ Animation/index.js
@@ -0,0 +1,2 @@
+TweenMax.to('.container_fire', 5, {repeat: -1, backgroundPosition: "0px 46680px", ease:SteppedEase.config(120)});
+TweenMax.to('.container_ice', 5, {repeat: -1, backgroundPosition: "0px -39864px", ease:SteppedEase.config(132)});
