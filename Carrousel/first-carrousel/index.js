b693e5e98128eafa20dde11e260ce74528e1adef
Mkheir13 <kheireddine.mederreg@ynov.com>
2022-06-10 19:58:15 +0200

    exercise 2

--- Carrousel/first-carrousel/index.js
+++ Carrousel/first-carrousel/index.js
@@ -21,5 +21,5 @@
     dots[i].className = dots[i].className.replace(" active", "");
 }
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    dots[slideIndex-1].className += "active";
 }
